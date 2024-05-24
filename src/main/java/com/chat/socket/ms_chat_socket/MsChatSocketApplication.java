b922aff3d62211e0718ccaaa2f82d66aa833f329
SturnioloBruno <brunosebastian.sturniolo@gmail.com>
2024-05-24 18:58:36 -0300

    endpoint de registro

---
+++ src/main/java/com/chat/socket/ms_chat_socket/MsChatSocketApplication.java
@@ -0,0 +1,13 @@
+package com.chat.socket.ms_chat_socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsChatSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsChatSocketApplication.class, args);
+	}
+
+}
